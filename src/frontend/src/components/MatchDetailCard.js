44bfe3c433628c2cc73de4255b8a6fdc8b0c930b
I-MOHIT <mohitchugh180919@gmail.com>
2022-06-19 21:49:27 +0530

    Added a frontend app with few hardcoded value components

---
+++ src/frontend/src/components/MatchDetailCard.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+
+export const MatchDetailCard = () => {
+  return (
+    <div className="MatchDetailCard">
+        <h3>Latest Matches</h3>
+        <h1>Match Details</h1>
+    </div>
+  );
+}
