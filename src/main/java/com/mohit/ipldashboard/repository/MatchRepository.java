892dfded8d65a22f2f693fe1473bc1695da5c396
I-MOHIT <mohitchugh180919@gmail.com>
2022-06-24 01:39:24 +0530

    Built Match API and Match frontend page

--- src/main/java/com/mohit/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/mohit/ipldashboard/repository/MatchRepository.java
@@ -4,12 +4,22 @@ import com.mohit.ipldashboard.model.Match;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
+
+    @Query("select m from Match m where (m.team1 = :teamName or m.team2 = :teamName) and m.date between :startDate and :endDate order by date desc")
+    List<Match> getMatchesByTeamBetweenDates(@Param("teamName") String teamName, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    // Very unoptimized way of doing data fetch/filtering
+    // List<Match> getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(String teamName1, LocalDate startDate1, LocalDate endDate1, String teamName2, LocalDate startDate2, LocalDate endDate2);
+
     default List<Match> findLatestMatchesByTeam(String teamName, int count) {
         return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
     }
